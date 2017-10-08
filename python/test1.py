cca0bc29922b3e5a3c3609beb474d1158d8c760d
Emil Fleron <emil.fleron@gmail.com>
2017-10-08 19:44:00 +0200

    changed to better names for problem file

--- python/test1.py
+++ python/test1.py
@@ -3,14 +3,14 @@ import os
 def test1():
     print "Testing OS.dir and feval"
     os.chdir("/bench")
-    x,y = oc.feval("prob1I",nout=2)
+    x,y = oc.feval("prob1aI",nout=2)
     print "1st", x
     print "2st", y
 
 def test2():
     print "Testing OS.dir and eval"
     os.chdir("/bench")
-    x,y = oc.eval("prob1I()",nout=2)
+    x,y = oc.eval("prob1aI()",nout=2)
     print "1st", x
     print "2st", y
 
