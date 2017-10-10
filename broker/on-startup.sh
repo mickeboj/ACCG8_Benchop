f84a701eb2ca9e903458a5675129339f9f1daecf
Mikael Nelsson <mikael.nelsson@live.se>
2017-10-10 14:24:35 +0200

    added missing in dockerfiles

--- broker/on-startup.sh
+++ broker/on-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rabbitmqctl add_user user1 pwd12
-rabbitmqctl add_vhost vost1
-rabbitmqctl set_permissions -p party-line guest ".*" ".*" ".*"
+rabbitmqctl add_vhost vhost1
+rabbitmqctl set_permissions -p vhost1 user1 ".*" ".*" ".*"
 rabbitmqctl set_user_tags user1 administrator
