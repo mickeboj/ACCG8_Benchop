10ee64ea654f509c044ddce5547aa6d505bee7bf
Mikael Nelsson <mikael.nelsson@live.se>
2017-10-10 14:10:13 +0200

    added flower

--- broker/on-startup.sh
+++ broker/on-startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rabbitmqctl add_vhost party-line
-rabbitmqctl set_permissions -p party-line guest "." "." ".*"
-echo "party-line vhost setup"
+rabbitmqctl add_user user1 pwd12
+rabbitmqctl add_vhost vost1
+rabbitmqctl set_permissions -p party-line guest ".*" ".*" ".*"
+rabbitmqctl set_user_tags user1 administrator
