95557eb34769020d11219a47e67863c52cc79948
Emil Fleron <emil.fleron@gmail.com>
2017-10-10 12:03:49 +0200

    Changed import and decleration of octave due to issues with running celery

--- cproj/tasks.py
+++ cproj/tasks.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 from .celery import app
-from oct2py import octave as oc
+from oct2py import Oct2Py
 
 @app.task
 def solveproblem(pn):
+    oc = Oct2Py()
     if not oc.pwd() == "/proj/bench":
         oc.chdir("bench/")
     x,y = oc.feval(pn,nout=2)
