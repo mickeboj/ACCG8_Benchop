77eb63b9925517029cce2140c03a2e80f0bda85a
Mikael Nelsson <mikael.nelsson@live.se>
2017-10-11 19:57:24 +0200

    missing s

--- flask/app.py
+++ flask/app.py
@@ -20,7 +20,7 @@ def solve_all_problems():
     results = group(solveproblem.s(PROBLEMS[i]) for i in xrange(len(PROBLEMS)))().get()
     ret_dic ={}
     for i in range(len(PROBLEMS)):
-        time , err = clean_res(result[i])
+        time , err = clean_res(results[i])
         dic ={}
         for j in range(len(METHODS)):
             dic[METHODS[j]] = {"time" : time[j] , "err" : err[j]}
