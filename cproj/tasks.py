a77c0b44f039b8f3d004a1e62cf27bdce08b146d
Mikael Nelsson <mikael.nelsson@live.se>
2017-10-14 19:44:00 +0200

    added .m files for parameter passing

--- cproj/tasks.py
+++ cproj/tasks.py
@@ -11,3 +11,15 @@ def solveproblem(pn):
     rel_err_reform = rel_err.tolist()
     time_reform = time.tolist()
     return rel_err_reform,time_reform
+
+
+@app.task
+def solveproblem_par(pn,par_dic):
+    oc = Oct2Py()
+    if not oc.pwd() == "/proj/bench":
+        oc.chdir("bench/")
+    
+    rel_err,time = oc.feval(pn,nout=2)
+    rel_err_reform = rel_err.tolist()
+    time_reform = time.tolist()
+    return rel_err_reform,time_reform
