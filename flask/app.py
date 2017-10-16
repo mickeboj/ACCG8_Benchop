5e2664b6907b75799f327036b8652c3b32d92ce9
Mikael Nelsson <mikael.nelsson@live.se>
2017-10-16 16:38:36 +0200

    now i fix? p11

--- flask/app.py
+++ flask/app.py
@@ -72,7 +72,7 @@ def make_rank_dic(m,dic,methods):
         for i in range(len(dic)):
             if dic[methods[i]][m] == "nan":
                 continue
-            if dic[methods[i]][m] < min_v and dic[methods[i]][m] > ret_l[-1].values()[0]):
+            if dic[methods[i]][m] < min_v and dic[methods[i]][m] > ret_l[-1].values()[0]:
                 min_v = dic[methods[i]][m]
                 ex_flag = False
                 p = i
